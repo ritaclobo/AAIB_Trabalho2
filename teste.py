ad3a93f5b75db95b508c120ec6a8e7d48df60d3e
ritaclobo <105011894+ritaclobo@users.noreply.github.com>
2022-11-24 16:50:26 +0000

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,7 +1,16 @@
 import streamlit as st
 import pandas as pd
 
-st.write("My First Streamlit Web App")
+st.write("Cloud Logger de Instrumentação")
+
+st.write("Gráfico")
+
+chart_data = pd.DataFrame(
+  np.random.randn(10,2)
+  columns =[f"Col{i+1}" for i in range(2)]
+)
+
+st.line_chart(chart_data)
 
 df = pd.DataFrame({"one": [1, 2, 3], "two": [4, 5, 6], "three": [7, 8, 9]})
 st.write(df)
