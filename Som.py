d9b5697655cc00f16972ef4e8f22160f892e8c6a
ritaclobo <105011894+ritaclobo@users.noreply.github.com>
2022-11-25 18:57:05 +0000

    Update Som.py

--- Som.py
+++ Som.py
@@ -57,6 +57,34 @@ sf_filewave = wave.open("c:/Users/Rita Lobo/Documents/Rita Lobo/Universidade - B
 signal_sf = sf_filewave.readframes(-1)
 soundwave_sf = numpy.frombuffer(signal_sf, dtype='int16')/len(data)
 
+# Mosquitto
+
+def connect_mqtt():
+    def on_connect(client, userdata, flags, rc):
+        if rc == 0:
+            print("Connected to MQTT Broker!")
+        else:
+            print("Failed to connect, return code %d\n", rc)
+
+    client = mqtt_client.Client(client_id)
+    #client.username_pw_set(username, password)
+    client.on_connect = on_connect
+    client.connect(broker, port)
+    return client
+
+def publish(client):
+    msg_count = 0
+    while True:
+        time.sleep(1)
+        msg = f"counter: {msg_count}"
+        result = client.publish(topic, msg)
+        # result: [0, 1]
+        status = result[0]
+        if status == 0:
+            print(f"Send `{msg}` to topic `{topic}`")
+        else:
+            print(f"Failed to send message to topic {topic}")
+        msg_count += 1
 
 
 client=mqtt.Client()
