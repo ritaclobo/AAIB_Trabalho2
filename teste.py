7fc6959e67ae4c91cff87b4e8fd371752419d8af
ritaclobo <105011894+ritaclobo@users.noreply.github.com>
2022-11-25 08:40:33 +0000

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import pandas as pd
 
+self.construct_sidebar(5)
+
 st.title("Cloud Logger de Instrumentação")
 
 st.write("Gráfico")
@@ -12,5 +14,3 @@ chart_data = pd.DataFrame(
 
 st.line_chart(chart_data)
 
-df = pd.DataFrame({"one": [1, 2, 3], "two": [4, 5, 6], "three": [7, 8, 9]})
-st.write(df)
