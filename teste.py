edf0f85c140416928a0cd0bc1bac7ba9efc18829
ritaclobo <105011894+ritaclobo@users.noreply.github.com>
2022-11-25 08:49:22 +0000

    Update teste.py

--- teste.py
+++ teste.py
@@ -5,6 +5,21 @@ self.construct_sidebar(5)
 
 st.title("Cloud Logger de Instrumentação")
 
+
+# Using object notation
+add_selectbox = st.sidebar.selectbox(
+    "How would you like to be contacted?",
+    ("Email", "Home phone", "Mobile phone")
+)
+
+# Using "with" notation
+with st.sidebar:
+    add_radio = st.radio(
+        "Choose a shipping method",
+        ("Standard (5-15 days)", "Express (2-5 days)")
+    )
+
+
 st.write("Gráfico")
 
 chart_data = pd.DataFrame(
