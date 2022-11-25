7470beab411ff8114e0641b3b6490dcb2b3431d9
ritaclobo <105011894+ritaclobo@users.noreply.github.com>
2022-11-25 09:25:21 +0000

    Update teste.py

--- teste.py
+++ teste.py
@@ -2,7 +2,36 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.title("Cloud Logger de Instrumentação")
+st.markdown(
+    """ 
+    <style>
+    .header-style{
+        font-size:25px;
+        font-family:sans-serif;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+st.markdown(
+    """ 
+    <style>
+    .font-style{
+        font-size:20px;
+        font-family:sans-serif;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+st.markdown(
+    '<p class="header-style"> Cloud Logger de Instrumentação </p>',
+    unsafe_allow_html=True
+)
+
+#st.title("Cloud Logger de Instrumentação")
 
 
 # Using object notation
