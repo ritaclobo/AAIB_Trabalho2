257cafb74a2f15b203c5988e09dc3cc0a3d5189a
ritaclobo <105011894+ritaclobo@users.noreply.github.com>
2022-12-01 11:37:59 +0000

    Update teste_randn.py

--- teste_randn.py
+++ teste_randn.py
@@ -8,54 +8,17 @@ from paho.mqtt import client as mqtt_client
 import threading
 from streamlit.runtime.scriptrunner.script_run_context import add_script_run_ctx
 
+st.title("Cloud Logger de Instrumentação")
 
-st.markdown(
-    """ 
-    <style>
-    .header-style{
-        font-size:25px;
-        font-family:sans-serif;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
-
-st.markdown(
-    """ 
-    <style>
-    .font-style{
-        font-size:20px;
-        font-family:sans-serif;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
-
-st.markdown(
-    '<p class="header-style"> Cloud Logger de Instrumentação </p>',
-    unsafe_allow_html=True
-)
-
-#st.title("Cloud Logger de Instrumentação")
-
-add_selectbox = st.sidebar.selectbox(
-    "Escolher a característica?",
-    ("Power", "Mean frequency", "Main frequency")
-)
+st.subheader("Comunicação de dados adquiridos através do MQTT Broker")
 
 with st.sidebar:
     st.write("Projeto desenvolvido para a disciplina de Aplicações Avançadas de Instrumentação Biomédica.")
     st.write("O botão Start permite começar a gravação de som com o computador durante um certo número de segundos pré-definido.")
-    st.write("Para acabar a aquisição pode carregar no botão de Stop.")
-    add_radio = st.radio(
-        "Escolher a característica",
-        ("Power", "Mean frequency")
-    )
+    st.write("Escolher a característica:")
+    checkbox_one = st.checkbox("Sonograma")
+    checkbox_two = st.checkbox("RMSE")
 
-st.write("Este primeiro gráfico represenda a amplitude da onda de som que foi gravada em função do tempo de gravação.")   
- 
 def plotd():
     df = pd.read_csv("outro_teste2.csv", header=None)
     df.index = ["Tempo", "Sound Wave", "Tempo RMSE" ,"RMSE"]
@@ -63,11 +26,14 @@ def plotd():
     st.line_chart(final_df, x = "Tempo", y="Sound Wave")
 
 graph = st.empty;
-plotd()
 
-st.write("O segundo gráfico representa a Energia RMS.")
+if checkbox_one:
+    st.write("Este primeiro gráfico represenda a amplitude da onda de som que foi gravada em função do tempo de gravação.")   
+    plotd()
 
-df = pd.read_csv("outro_teste2.csv", header=None)
-df.index = ["Tempo", "Sound Wave", "Tempo RMSE" ,"RMSE"]
-final_df=df.T
-st.line_chart(final_df, x = "Tempo RMSE", y="RMSE")
+if checkbox_two:
+    st.write("O segundo gráfico representa a Energia RMS")
+    df = pd.read_csv("outro_teste2.csv", header=None)
+    df.index = ["Tempo", "Sound Wave", "Tempo RMSE" ,"RMSE"]
+    final_df=df.T
+    st.line_chart(final_df, x = "Tempo RMSE", y="RMSE")
